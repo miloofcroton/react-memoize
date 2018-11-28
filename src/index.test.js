a14efdebf0c63d32fec88a11bde57db68c0f7915
Jack <ajackofsometrades@gmail.com>
2018-11-28 02:12:28 -0800

    it prevents the second calling with the same args

--- src/index.test.js
+++ src/index.test.js
@@ -10,15 +10,17 @@ describe('memoize(fn, equality)', () => {
 
   test('memoize calls the function it is given', () => {
     const func = jest.fn();
-    const memo = memoize(func)();
+    const memo = memoize(func);
+    memo();
     expect(func.mock.calls.length).toBe(1);
   });
 
   test('memoize prevents second calling with same args', () => {
-
-
-
-
+    const func = jest.fn();
+    const memo = memoize(func);
+    memo();
+    memo();
+    expect(func.mock.calls.length).toBe(1);
   });
 
   test('memoize allows second with different args', () => {
