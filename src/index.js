3b1194cc5184d743c2e583286fa8d8ab1fbc9787
Jack <ajackofsometrades@gmail.com>
2018-11-28 02:09:51 -0800

    it calls the passed in function

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 export const memoize = (fn, equality) => {
 
+  let lastArgs;
+  let lastResult;
 
   return function() {
+    const args = [...arguments];
+    const argString = args.join('--');
+    if(lastArgs == argString) return lastResult;
 
-  }
-
+    lastArgs = argString;
+    const result = fn.apply(null, args);
+    return result;
+  };
 };
 
 export const areEqual = (itemA, itemB) => {
--- src/index.test.js
+++ src/index.test.js
@@ -8,5 +8,24 @@ describe('memoize(fn, equality)', () => {
     expect(memo).toBeInstanceOf(Function);
   });
 
+  test('memoize calls the function it is given', () => {
+    const func = jest.fn();
+    const memo = memoize(func)();
+    expect(func.mock.calls.length).toBe(1);
+  });
+
+  test('memoize prevents second calling with same args', () => {
+
+
+
+
+  });
+
+  test('memoize allows second with different args', () => {
+
+
+
+  });
+
 
 });
